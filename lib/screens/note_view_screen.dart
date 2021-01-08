698b84c95f5d4e446c0ac11c680502b190b5c875
shahi <shahidfarid4577@gmail.com>
2021-01-08 21:27:52 +0500

    create notes using (text and images) with SQFlite and Firebase

--- lib/screens/note_view_screen.dart
+++ lib/screens/note_view_screen.dart
@@ -10,6 +10,44 @@ class NoteViewScreen extends StatefulWidget {
 class _NoteViewScreenState extends State {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder(
+      future: Provider.of<NoteProvider>(context,listen: false).getNotes(),
+      builder: (context,snapshot)
+      {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        else
+        {
+          if(snapshot.connectionState == ConnectionState.done)
+          {
+            return Scaffold(
+              body: Consumer<NoteProvider>(
+                child: noNotesUI(context),
+                builder: (context, noteprovider, child) =>
+                noteprovider.items.length <= 0
+                    ? child
+                    : Container(),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  goToNoteEditScreen(context);
+                },
+                child: Icon(Icons.add),
+              ),
+            );
+          }
+          return Container(
+            width: 0.0,
+            height: 0.0,
+          );
+        }
+
+      },
+    );
   }
 }
